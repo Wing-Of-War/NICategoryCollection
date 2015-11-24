07e96cfc297a86674683a7c74c34dd93fcc0a502
nomemo <wing.of.war.1980th@gmail.com>
2015-11-24 11:42:25 +0800

    Project init

---
+++ NICategoryCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NICategoryCollection
+//
+//  Created by 张珏 on 15/11/24.
+//  Copyright © 2015年 nomemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
